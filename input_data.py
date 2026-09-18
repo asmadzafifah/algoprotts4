e63bf9f32b6e071a2baf6d61bcf98e0716c7c890
sesaknafas <asmadzakiyyah16@gmail.com>
2026-09-18 03:34:48 -0700

    Update input_data.py

--- input_data.py
+++ input_data.py
@@ -8,7 +8,7 @@ Original file is located at
 """
 
 def input_produk_agro():
-    print("=== FORM INPUT PRODUK AGROINDUSTRI ===")
+    print("---------------- FORM INPUT DATA PRODUK (VERSI B) ----------------")
 
     nama_produk = input("Nama Produk (cth: Minyak Kelapa Sawit, Kopi Arabika): ")
     kategori = input("Kategori (cth: Perkebunan, Peternakan, Pangan Olahan): ")
